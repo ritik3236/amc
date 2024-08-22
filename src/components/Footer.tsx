133b8a65689790ecb5e36179d012e877bfca852e
Ritik <Ritikkushwaha1234@gmail.com>
2024-08-23 04:54:05 +0530

    Fixed Site Config

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -51,8 +51,8 @@ export default function Footer() {
                 </div>
                 <hr className="my-6 border-dashed border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8"/>
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">© 2023 <a
-                        className="hover:underline" href="https://flowbite.com/">Flowbite™</a>. All Rights Reserved.
+                    <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">© 2024 <a
+                        className="hover:underline" href="https://flowbite.com/">CoinDhan Pay™</a>. All Rights Reserved.
                     </span>
                     <div className="mt-4 flex sm:mt-0 sm:justify-center">
                         <a className="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="https://facebook.com/flowbite">
